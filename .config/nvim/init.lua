9b26f5dc529518a1bfb3adf14bda4a65f533426c
spacebear <144076611+grizznaut@users.noreply.github.com>
2024-04-02 23:16:50 -0400

    [nvim] fix fidget.nvim background

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -88,7 +88,16 @@ require('lazy').setup({
 
       -- Useful status updates for LSP
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim',       opts = {} },
+      {
+        'j-hui/fidget.nvim',
+        opts = {
+          notification = {
+            window = {
+              winblend = 0, -- Background color opacity in the notification window
+            }
+          }
+        }
+      },
 
       -- Additional lua configuration, makes nvim stuff amazing!
       'folke/neodev.nvim',
